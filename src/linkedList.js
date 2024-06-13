88bb7a9f55baabc80073f85cca4c52913d4255f8
nufc-hub <kmchenouf@gmail.com>
2024-06-13 22:52:07 +0900

    Add method templates

--- src/linkedList.js
+++ src/linkedList.js
@@ -2,4 +2,34 @@ class LinkedList {
   constructor(head = null) {
     this.head = head;
   }
+
+  // Adds a new node containing -value- to the end of the list.
+  append(value) {}
+
+  // Adds a new node containing -value- to the start of the list.
+  prepend(value) {}
+
+  // Returns the total number of nodes in the list
+  size() {}
+
+  // Returns the first node in the list.
+  head() {}
+
+  // Returns the last node in the list.
+  tail() {}
+
+  // Returns the node at the given index
+  at(index) {}
+
+  // Removes the last element from the list
+  pop() {}
+
+  // Returns true if the passed in value is in the list and otherwise returns false.
+  contains(value) {}
+
+  // Returns the index of the node containing value, or null if not found.
+  find(value) {}
+
+  // Represents your LinkedList objects as strings, so you can print them out and preview them in the console. The format should be: ( value ) -> ( value ) -> ( value ) -> null
+  toString() {}
 }
